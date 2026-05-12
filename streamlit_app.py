5af23a14cf6fe5c883d0b3b046af3d3269e57e10
rhainazea <rhainazea@gmail.com>
2026-05-12 09:10:29 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,13 @@ st.title("🎈 Perhitungan pH")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-#input bilangan
-n=int(input("Masukkan bilangan:"))
-#nilai awal faktorial
-faktorial=1
-#perhitungan faktorial
-for i in range(1,n+1):
-    faktorial *=i
-#output hasil 
-print("Faktorial dari",n,"adalah",faktorial)
+# Input bilangan
+angka = st.number_input("Masukkan bilangan bulat positif",min_value=0,step=1)
+# Tombol hitung
+if st.button("Hitung Faktorial"):
+    faktorial = 1
+# Perhitungan faktorial
+for i in range(1, angka + 1):
+    faktorial *= i
+# Menampilkan hasil
+st.success(f"Faktorial dari {angka} adalah {faktorial}")
